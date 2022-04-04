22ab4da577ec7f09a53482ad326aab9f38c7852e
Leonardo Araujo <leo.san9@hotmail.com>
2022-04-04 15:55:04 -0300

    botão reset

--- script/index.js
+++ script/index.js
@@ -1,18 +1,21 @@
 const tabuleiro = document.getElementById('tabuleiro')
 const reset = document.getElementById('reset')
-
+const casas = document.getElementById('casa');
+//  TABULEIRO
 const velha = {
     casas: ['', '', '', '', '', '', '', '', ''],
     simbolos: {
         opções: ['X', 'O'],
         mudar_index: 0,
         }
-
 }
-
+//  JOGO
 for (let i = 0; i < velha.casas.length; i++){
     let button = document.createElement('button')
     button.setAttribute('id','casa')
+    reset.addEventListener('click', () => {
+        button.innerText = ''
+    })
     button.addEventListener('click', () => {    
         button.innerText = velha.simbolos.opções[velha.simbolos.mudar_index]
         velha.simbolos.mudar_index = (velha.simbolos.mudar_index === 0 ? 1 : 0)
